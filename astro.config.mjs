11f3a217bbec231f5bf06b28c267d231c6bc1a24
A-coder152 <abdullahalsaleem19@gmail.com>
2026-06-02 16:33:29 -0400

    take 3 at github action thing

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,8 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
 // https://astro.build/config
-console.log('GITHUB_ACTIONS:', process.env.GITHUB_ACTIONS);
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/se31-website/' : '/',
+  base: '/',
   vite: {
     plugins: [tailwindcss()],
     css: {
