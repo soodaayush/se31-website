84bb4b8c0c24c4912cadd9dbd389b2c9ff879743
A-coder152 <abdullahalsaleem19@gmail.com>
2026-06-02 16:13:45 -0400

    fix: dark mode actually works

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/se31-website/',
+  base: process.env.NODE_ENV === 'production' ? '/se31-website/' : '/',
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    css: {
+      postcss: {
+        plugins: [],
+      },
+    },
   },
-
   integrations: [react()]
 });
