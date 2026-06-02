179ec19f4d96a0b0e9eea0f52fc2b62f97c02eb4
A-coder152 <abdullahalsaleem19@gmail.com>
2026-06-02 16:29:37 -0400

    fix: github action please be detectable

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
 // https://astro.build/config
+console.log('GITHUB_ACTIONS:', process.env.GITHUB_ACTIONS);
 export default defineConfig({
   base: process.env.GITHUB_ACTIONS ? '/se31-website/' : '/',
   vite: {
