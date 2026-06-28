70fd3c539803bf3da7e3601682e290ec903dbf91
AdvancedForge <156943520+AdvancedForge@users.noreply.github.com>
2026-06-27 22:37:49 -0400

    Add announcements interface and sample data

---
+++ src/data/announcements.ts
@@ -0,0 +1,14 @@
+export interface Announcement {
+  title: string;
+  date: string;
+  content: string;
+}
+
+export const ANNOUNCEMENTS: Announcement[] = [
+  {
+    title: "Welcome to SE31 Hub",
+    date: "2026-06-27",
+    content:
+      "Welcome! This page will be used to post important updates for the cohort.",
+  },
+];
